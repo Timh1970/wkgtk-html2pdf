91b8038f3c29bbbb3d23729a139788de18077014
Tim <tim@jtek.co.uk>
2026-03-12 20:28:20 +0000

    Bump version to 0.1.3

--- src/wk2gtkpdf/ichtmltopdf_int.cpp
+++ src/wk2gtkpdf/ichtmltopdf_int.cpp
@@ -11,7 +11,7 @@
 #include <systemd/sd-bus.h>
 #include <thread>
 #include <wayland-client.h>
-
+#define USE_WEBKIT_6
 #ifdef USE_WEBKIT_6
 #include <webkit/webkit.h>
 #else
@@ -29,7 +29,20 @@ struct WKGTK_init_impl {
 
 WKGTK_init::WKGTK_init()
     : m_pimpl(new WKGTK_init_impl()) {
+#ifdef USE_WEBKIT_6
+    // 1. WebKit6 / GTK4 is a "Snotty Cow" about headless environments.
+    // We must force the Sandbox OFF so it can see the Xvfb display socket.
+    setenv("WEBKIT_FORCE_SANDBOX", "0", 1);
 
+    // 2. Disable the Compositor and Hardware Acceleration for Xvfb.
+    // This stops the EGL/DRI2 warnings from becoming fatal Trace Traps.
+    setenv("WEBKIT_DISABLE_COMPOSITING_MODE", "1", 1);
+
+    // 3. Fake a D-Bus address if one isn't present to stop the "dbus-launch" error.
+    if (!getenv("DBUS_SESSION_BUS_ADDRESS")) {
+        setenv("DBUS_SESSION_BUS_ADDRESS", "unix:path=/dev/null", 0);
+    }
+#endif
     WKGTK_init_impl *p   = m_pimpl;
     m_pimpl->glob_Thread = std::thread([p]() {
         p->glob_loop = g_main_loop_new(nullptr, false);
